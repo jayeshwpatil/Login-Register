bc98a34c1fa08e01589e1743e01a5ebcae46a8e0
jayeshwpatil <jayeshwpatil@gmail.com>
2024-08-30 19:00:02 +0530

    added base url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  Base : ‘/Login-Register/’ ,  
+  base: '/Login-Register/', //name of git repository
   plugins: [
     vue(),
   ],
